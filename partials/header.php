44b0e6cb871d81df373b528f635ca1a861b42420
duykhang28 <101155523+duykhang288@users.noreply.github.com>
2022-04-08 20:48:49 +0700

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -8,7 +8,7 @@ include __DIR__ . '/../functions.php'; ?><!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" media="all" href="css/style.css">
+	
 	<title><?php
 	if (defined('TITLE')) {
 		echo TITLE;
