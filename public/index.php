fffabaaf671179f8cc101f658f23169e50260ee2
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2022-04-11 21:57:14 +0700

    update index

--- public/index.php
+++ public/index.php
@@ -4,22 +4,22 @@ include '../partials/header.php';
 
 include '../partials/mysqli_connect.php';
 
-if (isset($_GET['random'])) {
-	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY RAND() DESC LIMIT 1';
-} elseif (isset($_GET['favorite'])) {
-	$query = 'SELECT id, quote, source, favorite FROM quotes WHERE favorite=1 ORDER BY RAND() DESC LIMIT 1';
-} else {
-	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY date_entered DESC LIMIT 1';
-}
+// if (isset($_GET['random'])) {
+// 	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY RAND() DESC LIMIT 1';
+// } elseif (isset($_GET['favorite'])) {
+// 	$query = 'SELECT id, quote, source, favorite FROM quotes WHERE favorite=1 ORDER BY RAND() DESC LIMIT 1';
+// } else {
+// 	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY date_entered DESC LIMIT 1';
+// }
 
-if ($result = mysqli_query($dbc, $query)) {
+// if ($result = mysqli_query($dbc, $query)) {
 
 	
 
-} else {
-	echo '<p class="error">Không thể lấy dữ liệu vì:<br>' . mysqli_error($dbc) .
-			'.</p><p>Câu truy vấn là: ' . $query . '</p>';
-}
+// } else {
+// 	echo '<p class="error">Không thể lấy dữ liệu vì:<br>' . mysqli_error($dbc) .
+// 			'.</p><p>Câu truy vấn là: ' . $query . '</p>';
+// }
 
 mysqli_close($dbc);
 
