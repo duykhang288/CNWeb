eabdee0333a557aebdce7b0ea7327fc67ab87c2b
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2022-04-14 14:24:19 +0700

    update product

--- partials/product.php
+++ partials/product.php
@@ -1,3 +1,26 @@
 <main class="container">
-  
+    <section class="typePhone">
+        <h3>Iphone</h3>
+        <div>
+
+        </div>
+    </section>
+    <section class="typePhone">
+        <h3>SamSung</h3>
+        <div>
+
+        </div>
+    </section>
+    <section class="typePhone">
+        <h3>Huawei</h3>
+        <div>
+
+        </div>
+    </section>
+    <section class="typePhone">
+        <h3>Oppo</h3>
+        <div>
+
+        </div>
+    </section>
 </main>
--- public/index.php
+++ public/index.php
@@ -4,28 +4,6 @@ include '../partials/header.php';
 
 include '../partials/mysqli_connect.php';
 
-<<<<<<< HEAD
-// if (isset($_GET['random'])) {
-// 	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY RAND() DESC LIMIT 1';
-// } elseif (isset($_GET['favorite'])) {
-// 	$query = 'SELECT id, quote, source, favorite FROM quotes WHERE favorite=1 ORDER BY RAND() DESC LIMIT 1';
-// } else {
-// 	$query = 'SELECT id, quote, source, favorite FROM quotes ORDER BY date_entered DESC LIMIT 1';
-// }
-
-// if ($result = mysqli_query($dbc, $query)) {
-
-	
-
-// } else {
-// 	echo '<p class="error">Không thể lấy dữ liệu vì:<br>' . mysqli_error($dbc) .
-// 			'.</p><p>Câu truy vấn là: ' . $query . '</p>';
-// }
-
-mysqli_close($dbc);
-
-include '../partials/product.php';
-=======
 ?>
 	<link rel="stylesheet" href="css/index.css">
 	
@@ -72,6 +50,8 @@ include '../partials/product.php';
 	</div>
 	
 <?php
->>>>>>> 530d82eb6335082977e262a3edfe45b33472436a
+
+include '../partials/product.php';
+
 include '../partials/footer.php';
 ?>
