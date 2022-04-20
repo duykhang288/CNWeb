356d8eb4051e2fb6519b16e9e49b43e3a8573f42
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2022-04-20 10:53:27 +0700

    update product

--- partials/product.php
+++ partials/product.php
@@ -1,11 +1,11 @@
 <div >
-    <!-- <section class="company">
+    <section class="company">
         <div>
             <h3>Apple</h3>
         </div>
         <div class="products row">
             <?php 
-                    $query = 'SELECT * FROM products ';
+                    $query = 'SELECT * FROM products where company="Apple"';
                     // ORDER BY date_entered DESC
                     if ($result = mysqli_query($dbc, $query)){
                         
@@ -43,31 +43,155 @@
                     //mysqli_close($dbc);
             ?>
         </div>
-    </section> -->
+    </section>
     
-    <!-- <section class="company">
+    <section class="company">
         <h3>SamSung</h3>
+        <div class="products row">
+            <?php 
+                    $query = 'SELECT * FROM products where company="SAMSUNG"';
+                    // ORDER BY date_entered DESC
+                    if ($result = mysqli_query($dbc, $query)){
+                        
+                        while ($row = mysqli_fetch_array($result)) {
+                    
+                            // $htmlspecialchars = 'htmlspecialchars';
+                            
+                            echo '<div class="product-item col-sm-3">
+                                <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+                                    <img class="card-img-top" src="'.$row['frontImage'].'">
+                                    <div class="card-body">
+                                        <h5 class="card-title">'.$row['proName'].'</h5>
+                                        <p>'.$row['company'].'</p>
+                                        <div class="price">'.$row['price'].' VNĐ</div>
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Yêu thích</button>
+                                            </div>
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Đặt Hàng</button>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>';
+                        
+                        }
+                    } else {
+                        echo '<p class="error">Không thể lấy dữ liệu vì: <br>' . mysqli_error($dbc) . 
+                                '.</p><p>Câu truy vấn là: ' . $query . '</p>';
+                    }
+                    
+                    //mysqli_close($dbc);
+            ?>
+        </div>
     </section>
     <section class="company">
         <h3>Huawei</h3>
+        <div class="products row">
+            <?php 
+                    $query = 'SELECT * FROM products where company="HUAWEI"';
+                    // ORDER BY date_entered DESC
+                    if ($result = mysqli_query($dbc, $query)){
+                        
+                        while ($row = mysqli_fetch_array($result)) {
+                    
+                            // $htmlspecialchars = 'htmlspecialchars';
+                            
+                            echo '<div class="product-item col-sm-3">
+                                <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+                                    <img class="card-img-top" src="'.$row['frontImage'].'">
+                                    <div class="card-body">
+                                        <h5 class="card-title">'.$row['proName'].'</h5>
+                                        <p>'.$row['company'].'</p>
+                                        <div class="price">'.$row['price'].' VNĐ</div>
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Yêu thích</button>
+                                            </div>
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Đặt Hàng</button>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>';
+                        
+                        }
+                    } else {
+                        echo '<p class="error">Không thể lấy dữ liệu vì: <br>' . mysqli_error($dbc) . 
+                                '.</p><p>Câu truy vấn là: ' . $query . '</p>';
+                    }
+                    
+                    //mysqli_close($dbc);
+            ?>
+        </div>
     </section>
     <section class="company">
         <h3>Oppo</h3>
-    </section> -->
-    <?php 
+        <div class="products row">
+            <?php 
+                    $query = 'SELECT * FROM products where company="OPPO"';
+                    // ORDER BY date_entered DESC
+                    if ($result = mysqli_query($dbc, $query)){
+                        
+                        while ($row = mysqli_fetch_array($result)) {
+                    
+                            // $htmlspecialchars = 'htmlspecialchars';
+                            
+                            echo '<div class="product-item col-sm-3">
+                                <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+                                    <img class="card-img-top" src="'.$row['frontImage'].'">
+                                    <div class="card-body">
+                                        <h5 class="card-title">'.$row['proName'].'</h5>
+                                        <p>'.$row['company'].'</p>
+                                        <div class="price">'.$row['price'].' VNĐ</div>
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Yêu thích</button>
+                                            </div>
+                                            <div class="col-6">
+                                                <button class="btn-custom btn">Đặt Hàng</button>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>';
+                        
+                        }
+                    } else {
+                        echo '<p class="error">Không thể lấy dữ liệu vì: <br>' . mysqli_error($dbc) . 
+                                '.</p><p>Câu truy vấn là: ' . $query . '</p>';
+                    }
+                    
+                    //mysqli_close($dbc);
+            ?>
+        </div>
+    </section>
+    <!-- <?php 
         $query = 'SELECT * FROM products ';
         if ($result = mysqli_query($dbc, $query)){
             $company="";
             while ($row = mysqli_fetch_array($result)) {
                 $queryCom= 'SELECT * FROM products where company="'.$row['company'].'";';
                 //$htmlspecialchars = 'htmlspecialchars';
-                if($company=mysqli_query($dbc, $queryCom)){
+                if($company!=$row['company']){
                     echo '
                     <section class="company">
                     <div>
                         <h3>'.$row['company'].'</h3>
                     </div>
                     <div class="products row">';
+                    $company=$row['company'];
+                }
+                if($company=mysqli_query($dbc, $queryCom)){
+                    
                     while ($row = mysqli_fetch_array($company)) {
                         echo '
                         <div class="product-item col-sm-3">
@@ -89,10 +213,9 @@
                             </div>
                         </div>';
                     }
-                    echo '</div>
-                        </section>';
-
                 }
+                echo '</div>
+                        </section>';
                 
             }
         } else {
@@ -101,5 +224,5 @@
         }
             
         mysqli_close($dbc);
-    ?>
+    ?> -->
 </div>
